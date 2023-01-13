3cceba9a3476d404a23d6b3d54a985ad64630f24
wjh2012 <wjh2012@naver.com>
2023-01-13 15:44:46 +0900

    Initial commit

---
+++ src/main/java/ggomg/myshopauth/MyshopAuthApplication.java
@@ -0,0 +1,13 @@
+package ggomg.myshopauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshopAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshopAuthApplication.class, args);
+	}
+
+}
